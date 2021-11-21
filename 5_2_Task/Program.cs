804afa2a532a41cbb159e3a7b614452690e90ce7
Администратор <@DESKTOP-PJPICF5>
2021-11-22 00:29:57 +0800

    Добавьте файлы проекта.

---
+++ 5_2_Task/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int N = Convert.ToInt32(Console.ReadLine());
+int[,] t = new int[N, N];
+int M = 0;
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        M++;
+        if ((M % 2) != 0)
+        {
+            t[i, j] = 1;
+            Console.Write("{0} ", t[i, j]);
+        }
+        else
+        {
+            t[i, j] = 0;
+            Console.Write("{0} ", t[i, j]);
+        }
+
+    }
+    Console.WriteLine();
+}
---
+++ 5_Task/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] array = new int[7];
+int N = 0;
+int M = 0;
+for (int i = 0; i < 7; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    N += array[i];
+    M = N /7;
+    Console.WriteLine("{0} ", array[i]);
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое {0}",M);
